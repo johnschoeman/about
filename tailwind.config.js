96bd9bc8e8c58b3e4c56c5108531361986656eef
John Schoeman <johnschoeman1617@gmail.com>
2021-08-02 08:30:42 -0400

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const production = !process.env.ROLLUP_WATCH;
+
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    content: ["./src/**/*.svelte"],
+    enabled: production, // disable purge in dev
+  },
+};
