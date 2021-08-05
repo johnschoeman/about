e31d98dcf12fda60a9c110f41bec183c444a657c
John Schoeman <johnschoeman1617@gmail.com>
2021-08-05 08:28:07 -0400

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
+  import { Router, Link, Route } from "svelte-routing"
+
   import Home from "./Home.svelte"
+  import About from "./About.svelte"
+  import Doodles from "./Doodles.svelte"
+  import Footer from "./Footer.svelte"
+
+  export let url = ""
 </script>
 
 <main>
-  <Home />
+  <Router bind:url>
+    <nav>
+      <h1><a href="/">John Schoeman</a></h1>
+
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/doodles">Doodles</Link>
+        <Link to="/about">About</Link>
+      </ul>
+    </nav>
+
+    <div>
+      <Route path="/doodles"><Doodles /></Route>
+      <Route path="/about"><About /></Route>
+      <Route path="/"><Home /></Route>
+    </div>
+  </Router>
+
+  <Footer />
 </main>
 
 <style global lang="postcss">
---
+++ src/Header.svelte
@@ -0,0 +1,9 @@
+<nav>
+  <h1><a href="/">John Schoeman</a></h1>
+
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/doodles">Doodles</a></li>
+    <li><a href="/about">About</a></li>
+  </ul>
+</nav>
